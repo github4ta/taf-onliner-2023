9d6216ba05e8eb02ddca35a42078985555b8506b
Yuliya-senkina <juliyasenkina@gmail.com>
2023-12-05 21:43:40 +0300

    add senkina 2

--- src/main/java/senkina/Senkina.java
+++ src/main/java/senkina/Senkina.java
@@ -2,6 +2,6 @@ package senkina;
 
 public class Senkina {
     public static void main(String[] args) {
-        System.out.println("Hello  Git!");
+        System.out.println("Hello Git !");
     }
 }
