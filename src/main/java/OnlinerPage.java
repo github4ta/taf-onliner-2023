995829fc26512bc77f7c5ef0b88e27fe5aa9f569
Dmitry-Ivanchikov <dmitry.ivanchikov@gmail.com>
2023-12-08 13:30:41 +0300

    add onliner page object

--- src/main/java/OnlinerPage.java
+++ src/main/java/OnlinerPage.java
@@ -1,4 +1,12 @@
 public class OnlinerPage {
-    String Url="https://www.onliner.by/";
-    String copyRightFooter ="//div[@class='footer-style__copy']";
+    private String Url = ("https://www.onliner.by/");
+    private String copyRightFooter = "//div[@class='footer-style__copy']";
+
+    public String getUrl() {
+        driver.get(Url);
+
+        public String getCopyRightFooter () {
+            return driver.findElement(By.xpath(copyRightFooter));
+        }
+    }
 }
