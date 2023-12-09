47b997b51184c772962b4f8199d61cbf0ba1b7f2
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2023-12-10 01:59:19 +0300

    add constructor BaraholkaPage

--- src/main/java/BaraholkaPage.java
+++ src/main/java/BaraholkaPage.java
@@ -1,6 +1,11 @@
 import org.openqa.selenium.WebDriver;
 
 public class BaraholkaPage {
-    private String enterTitle ="//h1[@class='m-title-i']";
+    private String enterTitle = "//h1[@class='m-title-i']";
     private WebDriver driver;
+
+    public BaraholkaPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
 }
