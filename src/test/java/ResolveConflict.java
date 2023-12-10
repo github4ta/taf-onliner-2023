9530f18a6cf34f5afa4251a8c30f31d2197d74e6
Tatiana_Pakulnitskaya <takovenya@gmail.com>
2023-12-10 13:13:37 +0300

    Resolve Conflick Pakulnitskaya

--- src/test/java/ResolveConflict.java
+++ src/test/java/ResolveConflict.java
@@ -1,16 +1,19 @@
 public class ResolveConflict {
+    public static String akachenok;
     public static String pakulnitskaya;
     public static String vorontsova;
-    public static String akachenok;
+
 
     public static String getPakulnitskaya() {
         return pakulnitskaya;
     }
 
-    public static String getVorontsova() {
-        return vorontsova;
-    }
     public static String getAkachenok() {
         return akachenok;
     }
+
+    public static String getVorontsova() {
+        return vorontsova;
+    }
 }
+
