7057670d801eeb73bfebd91bde077129cc0403ac
Anfiar <artykishkevich@gmail.com>
2023-12-10 13:04:01 +0300

    resolve conflicts

--- src/test/java/ResolveConflict.java
+++ src/test/java/ResolveConflict.java
@@ -1,6 +1,11 @@
 public class ResolveConflict {
     public static String vorontsova;
+    public static String kishkevich;
+
     public static String getVorontsova() {
        return vorontsova;
+    }
+    public static String getKishkevich() {
+        return kishkevich;
     }
 }
