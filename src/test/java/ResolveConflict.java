a19069386069cb5c0e43d93af7005d216192fd18
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2023-12-10 13:14:24 +0300

    add getBakumenko()

--- src/test/java/ResolveConflict.java
+++ src/test/java/ResolveConflict.java
@@ -1,15 +1,19 @@
 public class ResolveConflict {
     public static String vorontsova;
     public static String akachenok;
+
     public static String getVorontsova() {
-       return vorontsova;
+        return vorontsova;
     }
+
     public static String getAkachenok() {
         return akachenok;
     }
+
     public static String bakumenko;
 
     public static String getBakumenko() {
         return bakumenko;
     }
+
 }
