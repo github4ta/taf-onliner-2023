ced5cd6e955bd5b7c7bcec64e65bd6ca2591a6e1
Tatiana_Pakulnitskaya <takovenya@gmail.com>
2023-12-10 13:08:33 +0300

    Add method

--- src/test/java/ResolveConflict.java
+++ src/test/java/ResolveConflict.java
@@ -1,6 +1,12 @@
 public class ResolveConflict {
     public static String pakulnitskaya;
+    public static String vorontsova;
+
     public static String getPakulnitskaya() {
         return pakulnitskaya;
+    }
+
+    public static String getVorontsova() {
+        return vorontsova;
     }
 }
