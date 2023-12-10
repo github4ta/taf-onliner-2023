da537a2368f7d58cd2911586e7486e9d175f393b
Elena-Rybalko <ieliena.rybalko@mail.ru>
2023-12-10 14:20:01 +0300

    ResolveConflict

--- src/test/java/ResolveConflict.java
+++ src/test/java/ResolveConflict.java
@@ -1,11 +1,8 @@
 public class ResolveConflict {
-    public static String vorontsova;
+
     public static String rybalko;
 
-    public static String getVorontsova() {
 
-        return vorontsova;
-    }
 
     public static String getRybalko() {
         return rybalko;
