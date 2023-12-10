3922fa4bea07893b733dc3efec58dcd12b29c22c
Aliaksandr-Myshkevich <mav2691668@gmail.com>
2023-12-10 13:38:46 +0300

    add myshkevich

--- src/test/java/ResolveConflict.java
+++ src/test/java/ResolveConflict.java
@@ -1,9 +1,8 @@
 public class ResolveConflict {
-    public static String vorontsova;
-    public static String akachenok;
+
     public static String myshkevich;
 
-    public static String getVorontsova() {
-       return vorontsova;
+    public static String getMyshkevich() {
+       return myshkevich;
     }
 }
