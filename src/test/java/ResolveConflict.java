4a412bf42773bb1d92a934f94fc0926e0b3fc42e
Yuliya-senkina <juliyasenkina@gmail.com>
2023-12-10 13:13:19 +0300

    Resolve Conflict senkina

--- src/test/java/ResolveConflict.java
+++ src/test/java/ResolveConflict.java
@@ -1,15 +1,14 @@
 public class ResolveConflict {
     public static String vorontsova;
     public static String akachenok;
+    public static String senkina;
+
     public static String getVorontsova() {
        return vorontsova;
     }
     public static String getAkachenok() {
         return akachenok;
     }
-    public static String senkina;
-
-
 
     public static String getSenkina() {
         return senkina;
