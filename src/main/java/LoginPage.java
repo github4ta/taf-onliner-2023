e4a1604fa96e4508495a30951dcdafdb55e191ed
Tatiana Pakulnitskaya <49642178+tatianapakulnitskaya@users.noreply.github.com>
2023-12-12 02:03:40 +0300

    Add textEnter locator for Submit form Header

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -4,4 +4,5 @@ public class LoginPage {
     private String inputPassword ="//input[@type='password']";
     private String buttonEnter = "//div[@id='auth-container']//button[@type='submit']";
     private String textEnter = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+
 }
