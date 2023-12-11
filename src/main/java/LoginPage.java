9c52363955e4731645236d7aac3a7ac1b7cc9881
Tatiana_Pakulnitskaya <takovenya@gmail.com>
2023-12-12 01:04:17 +0300

    Add textEnter locator for Submit form Header

--- src/main/java/LoginPage.java
+++
@@ -1,6 +0,0 @@
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-
-public class LoginPage {
-    private String inputPassword ="//input[@type='password']";
-    private String buttonEnter = "//div[@id='auth-container']//button[@type='submit']";
-}
