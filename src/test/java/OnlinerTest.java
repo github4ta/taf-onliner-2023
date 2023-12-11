924c7e7e0aeb970ac55bcb1011a9eda6713e4a26
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2023-12-12 00:39:14 +0300

    add testFooterSite()

--- src/test/java/OnlinerTest.java
+++ src/test/java/OnlinerTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
 public class OnlinerTest extends BaseTest {
+    @Test
+    public void testFooterSite() {
+        driver.get("https://www.onliner.by/");
+        OnlinerPage onlinerPage = new OnlinerPage(driver);
+        onlinerPage.getCopyRightFooter();
+        Assertions.assertEquals("© 2001—2023 Onlíner", onlinerPage.getCopyRightFooter());
+    }
+
 }
