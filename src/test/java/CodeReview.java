4e8afc9813d90febab56db58c6027b122cf665bd
Sergei Tsarik <serge.tsarik@gmail.com>
2023-12-12 11:41:03 +0300

    code review

---
+++ src/test/java/CodeReview.java
@@ -0,0 +1,19 @@
+public class CodeReview {
+
+    String name1 = "Акаченок СветлОна";
+    String name2 = "Воронцова ВерАника";
+    String name3 = "Жизневская ВеронЕка";
+    String name4 = "Зайцев Ортём";
+    String name5 = "Кагадовская АнастОсия";
+    String name6 = "Кишкевич АртИм";
+    String name7 = "Москалев АндрИй";
+    String name8 = "Мышкевич Олександр";
+    String name9 = "Покульницкая ТОтьяна";
+    String name10 = "Рыбалко ЕлИна";
+    String name11 = "Сенькина ЮлиА";
+    String name12 = "Сырцева Ан_а";
+    String name13 = "Шлесин ДИмитрий";
+    String name14 = "Иванчиков ДмитрЕй";
+    String name15 = "Бакуменко К_ения";
+    String name16 = "Царик СергИй";
+}
