5efef2549c88bef7ba3cb787fe989f3cde954ccc
Yuliya-senkina <juliyasenkina@gmail.com>
2023-12-13 21:30:06 +0300

    add

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -6,17 +6,4 @@ public class LoginPage {
     private String inputPassword ="//input[@type='password']";
     private String buttonEnter = "//div[@id='auth-container']//button[@type='submit']";
     private String textEnter = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
-    private String inputButtonLoginUserAccount = "//div[@class='auth-bar__item auth-bar__item--text']";
-    private String titleLoginForm = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
-    WebDriver driver;
-    public LoginPage(WebDriver driver) {
-        this.driver = driver;
-    }
-    public void clickInputButtonLoginUserAccount() {
-
-        driver.findElement(By.xpath(inputButtonLoginUserAccount)).click();
-    }
-    public String getTitleLoginForm() {
-        return driver.findElement(By.xpath(titleLoginForm)).getText();
-    }
-}
+   }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,13 +3,5 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LoginTest extends BaseTest {
-    @Test
-    @DisplayName("UI-0200")
-    public void testOpeningLoginFormTitle() {
-        driver.get("https://www.onliner.by/");
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.clickInputButtonLoginUserAccount();
-        Assertions.assertEquals("Вход", loginPage.getTitleLoginForm());
-    }
 
 }
