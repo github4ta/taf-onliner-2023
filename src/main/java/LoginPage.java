b9ea8d4df683ac840aab15bb61f39f443c4beeb9
AnnaSyrtseva <77345297+AnnaSyrtseva@users.noreply.github.com>
2023-12-14 20:22:33 +0100

    UI-0201
    Co-authored-by: Алексей Сырцев <aleksei.turchin@gmail.com>

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -6,4 +6,6 @@ public class LoginPage {
     private String inputPassword ="//input[@type='password']";
     private String buttonEnter = "//div[@id='auth-container']//button[@type='submit']";
     private String textEnter = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    private String textLoginError = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    private String textPasswordError = "//*[@id='auth-container']/div/div[2]/div/form/div[2]/div/div/div[2]/div";
    }
