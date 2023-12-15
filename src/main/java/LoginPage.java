554187e3e163b1ca7e9970fb7324bdc173084860
Aliaksandr-Myshkevich <mav2691668@gmail.com>
2023-12-15 15:19:50 +0300

    add inputFieldNikName for LoginPage

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,9 +1,27 @@
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage {
     private String inputPassword ="//input[@type='password']";
     private String buttonEnter = "//div[@id='auth-container']//button[@type='submit']";
     private String textEnter = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    private String inputFieldNikName = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div";
+    private String sendKeysInputFieldNikName = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickInputFieldNikName() {
+        driver.findElement(By.xpath(inputFieldNikName)).click();
+    }
+
+    public String sendKeysInputFieldNikName(String test) {
+        WebElement inputFieldNikName = driver.findElement(By.xpath(sendKeysInputFieldNikName));
+        inputFieldNikName.sendKeys(test);
+        return inputFieldNikName.getText();
+    }
    }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,4 +4,14 @@ import org.junit.jupiter.api.Test;
 
 public class LoginTest extends BaseTest {
 
+    @Test
+    @DisplayName("UI-0101")
+    public void testInputFieldNikName() {
+
+        driver.get("https://www.onliner.by/");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.clickInputFieldNikName();
+        loginPage.sendKeysInputFieldNikName("Ник");
+
+    }
 }
