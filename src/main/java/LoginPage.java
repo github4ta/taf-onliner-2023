9583f7c25d179cb1fb8982ae2bfeb60b2857892c
Yuliya Senkina <141929919+Yuliya-senkina@users.noreply.github.com>
2023-12-16 11:02:52 +0300

    UI-0200
    * add testOpeningLoginFormTitle Senkina
    
    * add testOpeningLoginFormTitle Senkina
    
    * add testOpeningLoginFormTitle Senkina
    
    ---------
    
    Co-authored-by: Sergei Tsarik <49017393+github4ta@users.noreply.github.com>

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,11 +1,26 @@
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-    private String inputPassword ="//input[@type='password']";
+    WebDriver driver;
+    private String inputPassword = "//input[@type='password']";
     private String buttonEnter = "//div[@id='auth-container']//button[@type='submit']";
     private String textEnter = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    private String inputButtonLoginUserAccount = "//div[@class='auth-bar__item auth-bar__item--text']";
+    private String titleLoginForm = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
     private String textLoginError = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
     private String textPasswordError = "//*[@id='auth-container']/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickInputButtonLoginUserAccount() {
+        driver.findElement(By.xpath(inputButtonLoginUserAccount)).click();
+    }
+
+    public String getTitleLoginForm() {
+        return driver.findElement(By.xpath(titleLoginForm)).getText();
+    }
+
    }
